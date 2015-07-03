e44ab8e5e765645cfb74375676a6d7459c03a6a6
Mikko Nylén <mnylen@reaktor.fi>
2015-07-03 20:01:26 +0300

    Store for search results

---
+++ src/stores/search.js
@@ -0,0 +1,15 @@
+const {createStore} = require('ffux'),
+      Bacon         = require('baconjs'),
+      bing          = require('../lib/bing')
+
+module.exports = createStore({
+  actions: ["search"],
+
+  state(initialQuery, {search}) {
+    const query   = search.toProperty(initialQuery).map('.trim.skipDuplicates')
+    const results = query.debounce(200).flatMapLatest(bing.search)
+    const await   = query.awaiting(results)
+
+    return Bacon.combineTemplate({ query, results, awaitingResults: await })
+  }
+})
